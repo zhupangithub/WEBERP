3abbe6f85516fa99fc6b14827a83b7bbd5dd5f96
zhupangithub <572152944@qq.com>
2020-01-01 22:33:39 +0800

    first commit

---
+++ Code/ERP.Web/App_Start/webstack/MvcHandleError.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using log4net;
+
+namespace ERP.Web
+{
+    public class MvcHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            ILog log = LogManager.GetLogger(filterContext.RequestContext.HttpContext.Request.Url.LocalPath);
+            log.Error(filterContext.Exception);
+            base.OnException(filterContext);
+        }
+    }
+}
---
+++ Code/ERP.Web/Areas/Mms/MmsAreaRegistration.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using System.Web.Http;
+using ERP.Web;
+
+namespace ERP.Web.Areas.Mms
+{
+    public class MmsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Mms";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                this.AreaName + "default",
+                this.AreaName + "/{controller}/{action}/{id}",
+                new { area = this.AreaName, controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "ERP.Web.Areas."+ this.AreaName + ".Controllers" }
+            );
+
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+                this.AreaName + "Api",
+                "api/" + this.AreaName + "/{controller}/{action}/{id}",
+                new { area = this.AreaName, action = RouteParameter.Optional, id = RouteParameter.Optional, namespaceName = new string[] { string.Format("ERP.Web.Areas.{0}.Controllers", this.AreaName) } },
+                new { action = new StartWithConstraint() }
+            );
+        }
+    }
+}
